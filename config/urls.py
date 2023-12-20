2ff319bdac0640ce7504938927d14ea4e4c22999
Hudaifa Saleh <hudaifa890@gmail.com>
2023-12-21 02:37:20 +0300

    Removed HomePageView route from config/urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
 from django.urls import path
 
-from babel.accounts.views import HomePageView
-
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", HomePageView.as_view(), name="home"),
 ]
