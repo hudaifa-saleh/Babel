05d37012fa891e3a82d9d9e0e6b070702d197a36
Hudaifa Saleh <hudaifa890@gmail.com>
2023-12-25 05:09:07 +0300

    Added oauth2_provider to Django settings and included its URLs in the main URL configuration

--- config/settings.py
+++ config/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "oauth2_provider",
     "django_extensions",
     "babel.accounts",
     "babel.moments",
--- config/urls.py
+++ config/urls.py
@@ -4,4 +4,5 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
+    path("o/", include("oauth2_provider.urls", namespace="oauth2_provider")),
 ]
